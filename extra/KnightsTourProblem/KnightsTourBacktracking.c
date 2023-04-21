eff20a0da82c90322fe33ee289d8d76b2c076806
Junio <jcf_ssp@hotmail.com>
2023-04-21 11:26:56 -0300

    review KnightsTourBacktracking

--- extra/KnightsTourProblem/KnightsTourBacktracking.c
+++ extra/KnightsTourProblem/KnightsTourBacktracking.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define N 5
+#define N 6
 
 #define INVERSE_MOVE
 
@@ -13,23 +13,23 @@ const int yMove[8] = { -1, -2, -2, -1, 1, 2, 2, 1 };
 #endif 
 
   
-int isSafe(int x, int y, int sol[N][N])
+int isSafe(int x, int y, int board[N][N])
 {
-    return (x >= 0 && x < N && y >= 0 && y < N && sol[x][y] == -1);
+    return (x >= 0 && x < N && y >= 0 && y < N && board[x][y] == -1);
 }
   
-void printSolution(int sol[N][N])
+void printMatrix(int board[N][N])
 {
     for (int x = 0; x < N; x++) 
     {
         for (int y = 0; y < N; y++)
-            printf(" %2d ", sol[x][y]);
+            printf(" %2d ", board[x][y]);
         printf("\n");
     }
 }
     
 // A recursive utility function to solve Knight Tour problem.
-int solveKTUtil(int x, int y, int move_i, int sol[N][N])
+int solveKTUtil(int x, int y, int move_i, int board[N][N])
 {
     int k, next_x, next_y;
 
@@ -39,13 +39,13 @@ int solveKTUtil(int x, int y, int move_i, int sol[N][N])
     {
         next_x = x + xMove[k];
         next_y = y + yMove[k];
-        if (isSafe(next_x, next_y, sol)) 
+        if (isSafe(next_x, next_y, board)) 
         {
-            sol[next_x][next_y] = move_i;
-            if (solveKTUtil(next_x, next_y, move_i + 1, sol) == 1)
+            board[next_x][next_y] = move_i;
+            if (solveKTUtil(next_x, next_y, move_i + 1, board) == 1)
                 return 1;
             else
-                sol[next_x][next_y] = -1; // backtracking
+                board[next_x][next_y] = -1; // backtracking
         }
     }
   
@@ -55,9 +55,8 @@ int solveKTUtil(int x, int y, int move_i, int sol[N][N])
 /* Driver Code */
 int main()
 {
-    int sol[N][N];
-    int x_start = 0;
-    int y_start = 0;
+    int board[N][N];
+	int sol[N][N];
     
     for (int x_start=0; x_start<N; x_start++)
     {
@@ -68,22 +67,27 @@ int main()
             /* Initialization of solution matrix */
             for (int x = 0; x < N; x++)
                 for (int y = 0; y < N; y++)
-                    sol[x][y] = -1;
+                    board[x][y] = -1;
             
             // Since the Knight is initially at the first block
-            sol[x_start][y_start] = 0;
+            board[x_start][y_start] = 0;
         
-            if (solveKTUtil(x_start, y_start, 1, sol) == 0) 
+            if (solveKTUtil(x_start, y_start, 1, board) == 0) 
             {
-                printf("\n-> Solution does not exist.\n");
+                printf(" -> Solution does not exist.\n");
+				sol[x_start][y_start] = 0;
             }
             else
             {
-                printf("\n-> Solution:\n");
-                printSolution(sol);
+                printf(" -> Solution:\n\n");
+				sol[x_start][y_start] = 1;
+                printMatrix(board);
             }
             printf("\n");
         }
     }
+	
+     printMatrix(sol);
+	 
     return 0;
 }
