0fc5e35d4f09a2a3f5211011aa206107734ae7c2
Junio <jcf_ssp@hotmail.com>
2023-05-16 20:00:01 -0300

    add exercise 13

---
+++ Exercise-13-BookShop/bookShop.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#define N 1000
+#define MAX(a, b) (a > b ? a : b)
+
+int main() 
+{
+    int n, x;
+    int h[N], s[N];
+    int dp[N + 1][N + 1];
+
+    scanf("%d %d", &n, &x);
+
+    for (int i = 0; i < n; i++) scanf("%d", &h[i]);
+
+    for (int i = 0; i < n; i++) scanf("%d", &s[i]);
+
+    for (int i = 0; i <= n; i++) 
+    {
+        dp[i][0] = 0;
+        dp[0][i] = 0;
+    }
+
+    for (int i = 0; i < n; i++) 
+    {
+        for (int j = 1; j <= x; j++) 
+        {
+            if (h[i] <= j)
+                dp[i+1][j] = MAX(s[i] + dp[i][j - h[i]], dp[i][j]);
+                
+            else 
+                dp[i+1][j] = dp[i][j];
+        }
+    }
+
+    printf("%d\n", dp[n][x]);
+
+    return 0;
+}
