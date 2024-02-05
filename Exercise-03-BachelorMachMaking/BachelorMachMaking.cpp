5457be0ee038f2f6233bbe78cb7d675650147ead
Junio Cesar Ferreira <jcf_ssp@hotmail.com>
2024-02-04 22:04:39 -0300

    review - correção da solução do exercicio 3

--- Exercise-03-BachelorMachMaking/BachelorMachMaking.cpp
+++ Exercise-03-BachelorMachMaking/BachelorMachMaking.cpp
@@ -5,13 +5,17 @@
 
 using namespace std;
 
-int findClosest(int age, const vector<int>& S, vector<bool>& used) {
+int findClosest(int age, const vector<int>& S, vector<bool>& used) 
+{
     int minDiff = INT_MAX;
     int closest = -1;
-    for (int i = 0; i < S.size(); i++) {
-        if (!used[i]) {
+    for (int i = 0; i < S.size(); i++) 
+    {
+        if (!used[i]) 
+        {
             int diff = abs(S[i] - age);
-            if (diff < minDiff) {
+            if (diff < minDiff) 
+            {
                 minDiff = diff;
                 closest = i;
             }
@@ -20,38 +24,39 @@ int findClosest(int age, const vector<int>& S, vector<bool>& used) {
     return closest;
 }
 
-int main() {
+int main() 
+{
     int cases = 0;
     int n, m;
-    while (cin >> n >> m) {
+    while (cin >> n >> m) 
+    {
         if (n == 0 && m == 0) break;
 
         vector<int> B(n), S(m);
         vector<bool> used(m, false);
         int minB = INT_MAX;
 
-        for (int& b : B) {
+        for (int& b : B) 
+        {
             cin >> b;
             minB = min(minB, b);
         }
-        for (int& s : S) {
+        for (int& s : S) 
             cin >> s;
-        }
 
         sort(B.begin(), B.end());
         sort(S.begin(), S.end());
 
-        for (int b : B) {
+        for (int b : B) 
+        {
             int closest = findClosest(b, S, used);
-            if (closest != -1) {
+            if (closest != -1) 
                 used[closest] = true;
-            }
         }
 
         int bachelorsLeft = n;
-        for (bool u : used) {
+        for (bool u : used) 
             if (u) bachelorsLeft--;
-        }
 
         cout << "Case " << ++cases << ": ";
         if (bachelorsLeft > 0) cout << bachelorsLeft << " " << minB << endl;
